501a56992ebbaf1c572f47abf6fc9f9708333379
Alex761944 <christen.alexander@web.de>
2025-11-10 13:43:46 +0100

    Use lucide react

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,73 +1,19 @@
 import "./Button.css";
+import { SquarePen, CheckSquare, Trash2 } from "lucide-react";
 
 export function Button({ icon, variant, onClick }) {
-  let iconMarkup = null;
+  const icons = {
+    edit: SquarePen,
+    checkmark: CheckSquare,
+    trashcan: Trash2,
+  };
 
-  if (icon === "edit") {
-    iconMarkup = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-        <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
-      </svg>
-    );
-  } else if (icon === "checkmark") {
-    iconMarkup = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M21 10.656V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h12.344" />
-        <path d="m9 11 3 3L22 4" />
-      </svg>
-    );
-  } else if (icon === "trashcan") {
-    iconMarkup = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M10 11v6" />
-        <path d="M14 11v6" />
-        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
-        <path d="M3 6h18" />
-        <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
-      </svg>
-    );
-  }
-
-  let classes = "Button";
-
-  if (variant === "icon") {
-    classes += " Button--Icon";
-  }
+  const IconComponent = icons[icon];
+  const classes = `Button${variant === "icon" ? " Button--Icon" : ""}`;
 
   return (
     <button className={classes} onClick={onClick}>
-      {icon ? iconMarkup : null}
+      {IconComponent && <IconComponent size={20} strokeWidth={2} />}
     </button>
   );
 }
